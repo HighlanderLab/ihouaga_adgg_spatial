0868935c4d0a71420a697af6215e57b5c871a240
Prof-ThiagoOliveira <thiago.paula.oliveira@usp.br>
2021-06-19 01:37:18 +0100

    Correlation_Var & MILK_YIELD & MILK 305 descriptive statistics

--- Scripts/Rscripts data cleaning HSTSA_revised.R
+++ Scripts/Rscripts data cleaning HSTSA_revised.R
@@ -15,6 +15,8 @@ library(ggplot2)
 library(ggExtra)
 library(EnvStats)
 library(lme4)
+library(readr)
+library(GGally)
 #=======================================================================
 # Uploading datasets
 #=======================================================================
@@ -61,7 +63,7 @@ dataHST$END_LACTATION_DTM <- as.Date(dataHST$END_LACTATION_DTM,
 #=======================================================================
 dataHST$PARTICIPANT[dataHST$PARTICIPANT == "Unknown"] <- NA
 #=======================================================================
-# Two columns to identify commercial and under study animals
+# Two columns to identify commercial and under stud animals
 #=======================================================================
 dataHST$Type <- str_extract_all(as.character(dataHST$PARTICIPANT),
                                  "[:upper:]", simplify = TRUE)
@@ -69,6 +71,16 @@ dataHST$Class <- paste0(dataHST$Type[, 1], dataHST$Type[, 2],
                          dataHST$Type[, 3])
 dataHST$Class <- as.factor(dataHST$Class)
 dataHST <- subset(dataHST,  select = -c(Type))
+#-----------------------------------------------------------------------
+dataHST$Herd <- readr::parse_number(as.character(dataHST$PARTICIPANT))
+#=======================================================================
+# Organizing the whole data set - removing NA and Zeros
+dataHST  <- dataHST %>%
+  filter(Class %in% c("HST", "MCM", "NA")) %>%
+  filter(is.na(MILK_YLD) == FALSE) %>%
+  filter(MILK_YLD > 0) %>%
+  droplevels()
+levels(dataHST$Class)[3] <- "Unclassified"
 #=======================================================================
 # Summary
 #=======================================================================
@@ -97,6 +109,30 @@ dataHST %>%
     SdMilk =  sqrt(VarMilk),
     nMilk = n()
   )
+
+dataHST %>%
+  filter(Class != "NA") %>%
+  group_by(Herd, Class) %>%
+  summarise(
+    MeanMilk = mean(MILK_YLD, na.rm = TRUE),
+    MedianMilk = median(MILK_YLD, na.rm = TRUE),
+    VarMilk = var(MILK_YLD, na.rm = TRUE),
+    SdMilk =  sqrt(VarMilk),
+    nMilk = n()
+  ) %>%
+  ggplot(aes(y = MeanMilk, x = SdMilk)) +
+  geom_point(shape = 1) +
+  facet_grid(~Class) +
+  xlab("SD(Milk)") +
+  ylab("Milk Average") +
+  theme_bw()
+ggsave("herd_mean_sd.png")
+
+# We have to investigate why there are herds with zero SD
+# You can talk on this in the lab meeting
+
+# There three observation in the HST to be inbestigated
+# observations above a Mean of 15000
 #-----------------------------------------------------------------------
 # Mean and Variance - groupd by Class and PARITY
 SMilk <- dataHST %>%
@@ -111,9 +147,8 @@ SMilk <- dataHST %>%
 
 # Mean vs. Variance
 SMilk %>%
-  ggplot(aes(y = MeanMilk, x = SdMilk,  colour = PARITY,
-             size = PARITY)) +
-  geom_point(shape = 1) +
+  ggplot(aes(y = MeanMilk, x = SdMilk,  colour = PARITY)) +
+  geom_text(aes(label = PARITY)) +
   xlab("SD(Milk)") +
   ylab("E(Milk)") +
   theme_bw()
@@ -121,16 +156,28 @@ SMilk %>%
 
 Mean vs. Variance grouped by Class
 SMilk %>%
-  ggplot(aes(y = MeanMilk, x = SdMilk,  colour = PARITY,
-             size = PARITY)) +
-  geom_point(shape = 1) +
+  ggplot(aes(y = MeanMilk, x = SdMilk,  label = PARITY)) +
+  geom_label(aes(fill = factor(PARITY)), colour = "white",
+             fontface = "bold") +
   facet_wrap(~Class) +
   xlab("SD(Milk)") +
   ylab("E(Milk)") +
   theme_bw()
-# Linear relationship between mean and variance -> HST and MCM
-# NAs group has variance more concentrate around 1900 and mean 3800
-# Few observations for JSE and UNK - remove from the dataset?
+ggsave("parity.png")
+# Here we can see:
+## HST: High mean and high variance compared to MCM and NA HST: Mean and
+## SD increases from Parity 1 to parity 3 and starts to decrease from
+## parity 4
+
+## MCM: Mean lower than HST but higher than NA
+## Distance between Parity 1 to 2 is higher than observed to HSTSA
+## Mean decrease is slower in MCM compared to HST -> you can see that
+## numbers 9 and 8 are close on MCM compared to HSTSA
+
+# NA: The lowest mean and SD. Why this is happeneing here? Which kind
+# of herd/animals we have as NA?? Seems a different group. If NA were
+# composed by animals from HST and MCM I expected a Mean-Variance
+# relationship between HST and MCM -> something to think!
 #-----------------------------------------------------------------------
 # Mean vs. number of observations
 SMilk %>%
@@ -173,29 +220,200 @@ SMilk %>%
 
 # Ok -> linear relationship
 #=======================================================================
-# BoxPlots
+# Centering the variable Milk
 #=======================================================================
 dataHST %>%
   mutate_at("PARITY", factor) %>%
-  ggplot(aes(y = MILK_YLD, x = PARITY)) +
-  geom_boxplot(na.rm = TRUE) +
+  mutate(MYc = MILK_YLD-mean(MILK_YLD)) %>%
+  ggplot(aes(y = MYc, x = PARITY)) +
+  geom_boxplot() +
   facet_grid(~ Class) +
+  xlab("Levels of Parity") +
+  ylab("Centred Milk Yield") +
   theme_bw()
 
 # Distribution is very assymetric for both HST and MCM class
 # We probably have to handle with estimation problem (high leverage)
-
-#NA has a mixture of animals from HST, MCM, UNK and JSE -> also
-# assymetric
 #=======================================================================
-# Testing basic initial models
+#=======================================================================
+# MILK 305
+#=======================================================================
+#=======================================================================
+# Stacking the data
+#=======================================================================
+dataHST <- rbind(dataHST1, dataHST2)
+dim(dataHST)[1] == dim(dataHST1)[1] + dim(dataHST2)[1] # Expect TRUE
+#=======================================================================
+# Organizing date variables
+#=======================================================================
+dataHST$CALVING_DTM <- as.Date(dataHST$CALVING_DTM,
+                                format = "%d/%m/%Y")
+dataHST$LACTATION_DTM <- as.Date(dataHST$LACTATION_DTM,
+                                  format = "%d/%m/%Y")
+dataHST$END_LACTATION_DTM <- as.Date(dataHST$END_LACTATION_DTM,
+                                      format = "%d/%m/%Y")
+#=======================================================================
+# Factor PARTICIPANT -> Unknown level to NA
+#=======================================================================
+dataHST$PARTICIPANT[dataHST$PARTICIPANT == "Unknown"] <- NA
+#=======================================================================
+# Two columns to identify commercial and under stud animals
+#=======================================================================
+dataHST$Type <- str_extract_all(as.character(dataHST$PARTICIPANT),
+                                 "[:upper:]", simplify = TRUE)
+dataHST$Class <- paste0(dataHST$Type[, 1], dataHST$Type[, 2],
+                         dataHST$Type[, 3])
+dataHST$Class <- as.factor(dataHST$Class)
+dataHST <- subset(dataHST,  select = -c(Type))
+#-----------------------------------------------------------------------
+dataHST$Herd <- readr::parse_number(as.character(dataHST$PARTICIPANT))
+#=======================================================================
+# Organizing the whole data set - removing NA and Zeros
+dataHST  <- dataHST %>%
+  filter(Class %in% c("HST", "MCM", "NA")) %>%
+  filter(is.na(MILK_YLD_305D) == FALSE) %>%
+  filter(MILK_YLD_305D > 0) %>%
+  droplevels()
+levels(dataHST$Class)[3] <- "Unclassified"
+#=======================================================================
+# Descriptive Statistics
+#=======================================================================
+# Mean and Variance - groupd by PARITY
+dataHST %>%
+  group_by(PARITY) %>%
+  summarise(
+    MeanMilk = mean(MILK_YLD_305D, na.rm = TRUE),
+    MedianMilk = median(MILK_YLD_305D, na.rm = TRUE),
+    VarMilk = var(MILK_YLD_305D, na.rm = TRUE),
+    SdMilk =  sqrt(VarMilk),
+    nMilk = n()
+    )
+#-----------------------------------------------------------------------
+# Mean and Variance - groupd by Class
+dataHST %>%
+  group_by(Class) %>%
+  summarise(
+    MeanMilk = mean(MILK_YLD_305D, na.rm = TRUE),
+    MedianMilk = median(MILK_YLD_305D, na.rm = TRUE),
+    VarMilk = var(MILK_YLD_305D, na.rm = TRUE),
+    SdMilk =  sqrt(VarMilk),
+    nMilk = n()
+  )
 
-# Linear regression
-m1 <- lmer(MILK_YLD ~ PARITY + Class + (1 | PARTICIPANT) +
-             (1 | PARTICIPANT:ANIMAL_NUMBER), data = dataHST)
+dataHST %>%
+  filter(Class != "NA") %>%
+  group_by(Herd, Class) %>%
+  summarise(
+    MeanMilk = mean(MILK_YLD_305D, na.rm = TRUE),
+    MedianMilk = median(MILK_YLD_305D, na.rm = TRUE),
+    VarMilk = var(MILK_YLD_305D, na.rm = TRUE),
+    SdMilk =  sqrt(VarMilk),
+    nMilk = n()
+  ) %>%
+  ggplot(aes(y = MeanMilk, x = SdMilk)) +
+  geom_point(shape = 1) +
+  facet_grid(~Class) +
+  xlab("SD(Milk)") +
+  ylab("Milk Average") +
+  theme_bw()
+ggsave("herd_mean_sd.png")
 
-# Gamma distribution
-m1 <- glmer(MILK_YLD ~ PARITY + Class + (1 | PARTICIPANT) +
-              (1 | PARTICIPANT:ANIMAL_NUMBER),  family = "Gamma",
-            data = dataHST)
+# We have to investigate why there are herds with zero SD
+# You can talk on this in the lab meeting -> Same as in MILK_YIELD
+
+#-----------------------------------------------------------------------
+# Mean and Variance - groupd by Class and PARITY
+SMilk <- dataHST %>%
+  group_by(PARITY, Class) %>%
+  summarise(
+    MeanMilk = mean(MILK_YLD_305D, na.rm = TRUE),
+    MedianMilk = median(MILK_YLD_305D, na.rm = TRUE),
+    VarMilk = var(MILK_YLD_305D, na.rm = TRUE),
+    SdMilk =  sqrt(VarMilk),
+    nMilk = n()
+  )
+
+# Mean vs. Variance
+SMilk %>%
+  ggplot(aes(y = MeanMilk, x = SdMilk,  colour = PARITY)) +
+  geom_text(aes(label = PARITY)) +
+  xlab("SD(Milk)") +
+  ylab("E(Milk)") +
+  theme_bw()
+# Linear relationship between mean and variance
+
+# Mean vs. Variance grouped by Class
+SMilk %>%
+  ggplot(aes(y = MeanMilk, x = SdMilk,  label = PARITY)) +
+  geom_label(aes(fill = factor(PARITY)), colour = "white",
+             fontface = "bold") +
+  facet_wrap(~Class) +
+  xlab("SD(Milk 305)") +
+  ylab("E(Milk 305)") +
+  theme_bw()
+ggsave("parity.png")
+# Almost the same as discussed to Milk_YIELD
+#-----------------------------------------------------------------------
+# Mean vs. number of observations
+SMilk %>%
+  ggplot(aes(y = MeanMilk, x = nMilk,  label = PARITY)) +
+  geom_label(aes(fill = factor(PARITY)), colour = "white",
+             fontface = "bold") +
+  facet_wrap(~Class) +
+  xlab("Number of Observations") +
+  ylab("E(Milk 305)") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# We can see the Milk 305 production increases up to PARITY 3-4 and then
+# starts to decrese. This is related to the combination of effect of
+# PARITY and number of observations
+
+# SD vs. number of observations
+SMilk %>%
+  ggplot(aes(y = SdMilk, x = nMilk,  label = PARITY)) +
+  geom_label(aes(fill = factor(PARITY)), colour = "white",
+             fontface = "bold") +
+  facet_wrap(~Class) +
+  xlab("Number of Observations") +
+  ylab("SD(Milk 305)") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# Same for Standard deviation
+#-----------------------------------------------------------------------
+# Mean and Median relationship
+SMilk %>%
+  ggplot(aes(y = MeanMilk,  x = MedianMilk)) +
+  geom_point(shape = 1) +
+  facet_wrap(~Class) +
+  geom_smooth(method = "lm", se = FALSE) +
+  xlab("Median(Milk 305)") +
+  ylab("E(Milk 305)") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# Ok -> linear relationship
+#=======================================================================
+# Centering the variable Milk 305
+#=======================================================================
+dataHST %>%
+  mutate_at("PARITY", factor) %>%
+  mutate(MYc = MILK_YLD_305D-mean(MILK_YLD_305D)) %>%
+  ggplot(aes(y = MYc, x = PARITY)) +
+  geom_boxplot() +
+  facet_grid(~ Class) +
+  xlab("Levels of Parity") +
+  ylab("Centred Milk 305 Yield") +
+  theme_bw()
+
+# Distribution is very assymetric for both HST and MCM class
+
+#=======================================================================
+# Pairs
+#=======================================================================
+ggpairs(dataHST[, c(5, 7:12)], aes(colour = factor(PARITY),
+                                   alpha = 0.4))
+ggsave("pairsPlot.png",  width = 14, height = 14)
+ggsave("pairsPlot.png",  width = 16, height = 16)
 #=======================================================================
